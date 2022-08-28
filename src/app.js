d692853eb1c59668f495ca09e78f61644b4ad358
Gabriel <gabrielcuinprof@gmail.com>
2022-08-28 14:30:05 -0300

    database, knex

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-const express = require("express");
+require('express-async-errors')
 
-const routes = require('./routes');
+const express = require("express");
+const routes = require("./routes");
+const AppError = require("./utils/AppError");
+const sqliteConnection = require('./database/sqlite');
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
+
+sqliteConnection();
+
+app.use((error, req, res, next) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.log(error);
+
+  return res.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 const PORT = 3330;
 app.listen(PORT, () => {
